cb557acc51ad1af27e16af48d83c96ac31bea594
brankonan <branko.kovacevic09@gmail.com>
2024-01-12 11:55:33 -0800

    zadatak1

---
+++ src/AnimalList.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function AnimalList() {
+  const items = [
+    {
+      id: 1,
+      vrsta: "ptica",
+      ime: "orao",
+      datum: new Date().toLocaleDateString(),
+    },
+    {
+      id: 2,
+      vrsta: "riba",
+      ime: "pirana",
+      datum: new Date().toLocaleDateString(),
+    },
+    {
+      id: 3,
+      vrsta: "sisar",
+      ime: "medved",
+      datum: new Date().toLocaleDateString(),
+    },
+    {
+      id: 4,
+      vrsta: "gmizavac",
+      ime: "zmija",
+      datum: new Date().toLocaleDateString(),
+    },
+    {
+      id: 5,
+      vrsta: "vodozemac",
+      ime: "dazdevnjak",
+      datum: new Date().toLocaleDateString(),
+    },
+  ];
+  return (
+    <div>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.vrsta},{item.ime},{item.datum}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default AnimalList;
