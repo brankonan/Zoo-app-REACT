36265bab14377df32f4c14b558b70fac2c473cc6
brankonan <branko.kovacevic09@gmail.com>
2024-01-15 12:05:05 -0800

    zadatak4 i 5

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -33,19 +33,63 @@ function AnimalList() {
       datum: null,
     },
   ]);
-  const removeItem = (index) => {
-    // console.log("lakjdf", items.length);
-    const newItems = items.filter((item, i) => i !== index);
-    // newItems.slice();
-    setItems(newItems);
-    // console.log(items.length);
+  const [newAnimal, setNewAnimal] = useState({
+    vrsta: "",
+    ime: "",
+    datum: "",
+  });
 
-    const moveToTop = (index) => {
-        const 
-    }
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItems([...items, newAnimal]);
+  };
+
+  const removeItem = (index) => {
+    const newItems = items.filter((item, i) => i !== index);
+    setItems(newItems);
+  };
+  const moveToTop = (index) => {
+    const movedItems = items.filter((item, i) => i !== index);
+    const newItems = [items[index], ...movedItems];
+    setItems(newItems);
+  };
   return (
     <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="vrsta">Vrsta</label>
+          <input
+            type="text"
+            id="vrsta"
+            name="vrsta"
+            value={newAnimal.vrsta}
+            onChange={(e) =>
+              setNewAnimal({ ...newAnimal, vrsta: e.target.value })
+            }
+          />
+          <label htmlFor="ime">Ime</label>
+          <input
+            type="text"
+            id="ime"
+            name="ime"
+            value={newAnimal.ime}
+            onChange={(e) =>
+              setNewAnimal({ ...newAnimal, ime: e.target.value })
+            }
+          />
+          <label htmlFor="datum">Datum</label>
+          <input
+            type="date"
+            id="datum"
+            name="datum"
+            value={newAnimal.datum}
+            onChange={(e) =>
+              setNewAnimal({ ...newAnimal, datum: e.target.value })
+            }
+          />
+          <button>Add-Animal</button>
+        </form>
+      </div>
       <table>
         <thead>
           <tr>
